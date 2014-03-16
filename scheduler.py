9b7d90464ca1cd2eda908088644824a50714d416
rabbanis <rabbanis@2794b107-68de-4277-a86a-c390ad1d69d3>
2014-03-16 04:10:14 +0000

    updating the script

--- scheduler.py
+++ scheduler.py
@@ -4,27 +4,33 @@ import argparse
 import subprocess
 import string
 import time,sys
-
+import random
 
 def main():
     
     parser = argparse.ArgumentParser()
 
     #Read the arguments
-    parser.add_argument("-d", help="IP address of the destination physical machine")
-    parser.add_argument("-id", help="ID of the VM you want to migrate")
+    parser.add_argument('-d','--destination', required=True, help="IP address of the destination physical machine")
+    
+    VMS_to_migrate = parser.add_mutually_exclusive_group(required=True)
+    VMS_to_migrate.add_argument("-id", help="Migrate only one vm with the <id>")
+    VMS_to_migrate.add_argument("-all", action='store_true', help="Migrate all VMs")
+    
+    #the policy used - random vs smart
+    parser.add_argument("-p","--policy",choices=['random','smart'],default='random',help="Policy of VM Migration")
 
     args = parser.parse_args()
-
-    print args.d
-    print args.id
-
-    #read the list of VMs
-    VM_list = read_VM_list()
     
-    #migrate all the VMs
-    migrate_all(VM_list,args.d)
+    if(args.all):
+        #read the list of VMs
+        VM_list = read_VM_list()
     
+        #migrate all the VMs
+        migrate_all(VM_list,args.destination,args.policy)
+    else:
+        migrate_one(args.id,args.destination)
+        
     #start the timer
     start_time = time.time()
         
@@ -59,11 +65,34 @@ def read_VM_list():
     
     
 #Function to migrate all the VMs
-def migrate_all(VM_list,destination):
+def migrate_all(VM_list,destination,policy):
+    
+    #print the policy
+    if(policy == 'random'):
+        migrate_random(VM_list,destination)
+    else:
+        migrate_smart(VM_list,destination)
+
+#Random Migration
+def migrate_random(VM_list,destination):
+
+    print("Migrating all VMs - The policy is Random")
+    
+    #randomizing
+    random.shuffle(VM_list)
     
     for VM in VM_list:
         subprocess.call(["echo","xm","migrate",VM,destination,"-l"])
-        
+    return
+
+#Smart Algorithm Migration    
+def migrate_smart(VM_list, destination):
+    
+    print("SMART ALGORITHM - NOT YET DONE")
+    return
+    
+def migrate_one(VM,destination):
+    subprocess.call(["echo","xm","migrate",VM,destination,"-l"])
     return
 
 if __name__ == "__main__":
