2c7c45a5f25cc2b8410666dd909885cf27d2e5bc
rabbanis <rabbanis@2794b107-68de-4277-a86a-c390ad1d69d3>
2014-03-18 01:25:12 +0000

    added basic TTL

--- scheduler.py
+++ scheduler.py
@@ -19,31 +19,25 @@ def main():
     
     #the policy used - random vs smart
     parser.add_argument("-p","--policy",choices=['random','smart'],default='random',help="Policy of VM Migration")
-
+    parser.add_argument("-ttl", help="The TTL input for migration", default=0.0, type=float)
+    
     args = parser.parse_args()
+
     
     if(args.all):
         #read the list of VMs
         VM_list = read_VM_list()
-    
-        #migrate all the VMs
-        migrate_all(VM_list,args.destination,args.policy)
+        migrate_all(VM_list,args.destination,args.policy,args.ttl)
     else:
         migrate_one(args.id,args.destination)
         
-    #start the timer
-    start_time = time.time()
-        
-    #elapsed time
-    elapsed_time = time.time() - start_time
-    print ("The elapsed time is " + str(elapsed_time))    
-    
+    return    
 
 #Method VM list    
 def read_VM_list():
     
     #outputs the list of VMs in the stdout
-    command_output = subprocess.Popen(["ls","-al"],stdout=subprocess.PIPE)
+    command_output = subprocess.Popen(["xm","list"],stdout=subprocess.PIPE)
     
     #initialize VM list
     VM_list = []
@@ -65,38 +59,78 @@ def read_VM_list():
     
     
 #Function to migrate all the VMs
-def migrate_all(VM_list,destination,policy):
+def migrate_all(VM_list,destination,policy,ttl):
     
     #print the policy
     if(policy == 'random'):
-        migrate_random(VM_list,destination)
+        migrate_random(VM_list,destination,ttl)
     else:
         migrate_smart(VM_list,destination)
-
+        
+    return
+    
 #Random Migration
-def migrate_random(VM_list,destination):
+def migrate_random(VM_list,destination,ttl):
 
     print("Migrating all VMs - The policy is Random")
     
     #randomizing
     random.shuffle(VM_list)
     
-    for VM in VM_list:
-        subprocess.call(["echo","xm","migrate",VM,destination,"-l"])
+    if(ttl == 0.0):
+        migrate_no_ttl(VM_list,destination)
+    else:
+        migrate_ttl(VM_list,destination,ttl)
+        
     return
 
+#Migrate without the TTL defined
+def migrate_no_ttl(VM_list,destination):        
+    #start the time
+    start_time = time.time()
+    
+    for VM in VM_list:
+        subprocess.Popen(["xm","migrate",VM,destination,"-l"])        
+        
+    elapsed_time = time.time() - start_time
+
+    print("The Elapsed time is "+elapsed_time)
+    return
+
+
+#Migrate the VMs with the TTL defined
+def migrate_ttl(VM_list,destination,ttl):    
+    print("Starting migration within a TTL of"+ttl)
+    elapsed_time = 0.0
+    for VM in VM_list:
+        if(elapsed_time < ttl)
+            start_time = time.time()
+            subprocess.Popen(["xm","migrate",VM,destination,"-l"])        
+            elapsed_time_process = time.time() - start_time
+            elapsed_time += elapsed_time_process
+            
+            if(elapsed_time <= ttl)
+                print("Migrated "+VM+" within the TTL of "+ttl)
+        else
+            break
+            
+    
+    print("The Elapsed time is "+elapsed_time)
+    return
+    
 #Smart Algorithm Migration    
 def migrate_smart(VM_list, destination):
-    
     print("SMART ALGORITHM - NOT YET DONE")
-    return
+    return 0
     
 def migrate_one(VM,destination):
-    subprocess.call(["echo","xm","migrate",VM,destination,"-l"])
+    subprocess.call(["xm","migrate",VM,destination,"-l"])
     return
+
 
 if __name__ == "__main__":
         main()
+        
         
         
 #TTL - optional parameter - 
